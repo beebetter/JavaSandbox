6e63d4e824d1446ba5f3b666578aec6361b95d0e
beeBetter <artem.ogarkov@gmail.com>
2017-04-23 23:36:18 +0300

    add Factoradic Algo for all permutation generating

--- src/main/java/AllPermutationsGenerator.java
+++ src/main/java/AllPermutationsGenerator.java
@@ -1,15 +1,14 @@
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 
 public class AllPermutationsGenerator {
-    public <T> void heapsRecAlgo (int n, List<T> arr) {
+    private <T> void generate(int n, List<T> arr) {
         if (n == 1) {
-            printAll(arr);
+            //printAll(arr);
             return;
         }
         for (int i = 0; i < n - 1; i++) {
-            heapsRecAlgo(n - 1, arr);
+            generate(n - 1, arr);
             T last = arr.get(n - 1);
             if (n % 2 == 0) {
                 arr.set(n - 1, arr.get(i));
@@ -19,7 +18,41 @@ public class AllPermutationsGenerator {
                 arr.set(0, last);
             }
         }
-        heapsRecAlgo(n - 1, arr);
+        generate(n - 1, arr);
+    }
+
+    public <T> void heapsRecAlgo(List<T> items) {
+        generate(items.size(), items);
+    }
+
+    private <T> List<T> getPermutation(int n, int k, List<T> items) {
+        int ind, m = k;
+        int[] permuted = new int[n];
+        int[] elems = new int[n];
+        for (int i = 0; i < n; i++) {
+            elems[i] = i;
+        }
+        for (int i = 0; i < n; i++) {
+            ind = m % (n - i);
+            m = m / (n - i);
+            permuted[i] = elems[ind];
+            elems[ind] = elems[n - i - 1];
+        }
+        List<T> permutation = new ArrayList<T>(items);
+        for (int i = 0; i < n; i++) {
+            permutation.set(permuted[i], items.get(i));
+        }
+        return permutation;
+    }
+
+    public <T> void factoradicAlgo(List<T> items) {
+        int fact = 1;
+        for (int i = 1; i <= items.size(); i++) {
+            fact *= i;
+        }
+        for (int i = 0; i < fact; i++) {
+            getPermutation(items.size(), i, items);
+        }
     }
 
     private <T> void printAll(List<T> arr) {
@@ -33,9 +66,23 @@ public class AllPermutationsGenerator {
         System.out.println();
     }
     public static void main(String[] args) {
-        List<Integer> arr = new ArrayList<Integer>(Arrays.asList(1, 2, 3, 4));
+        List<Integer> arr = new ArrayList<Integer>();
+        for (int i = 1; i <= 10; i++) {
+            arr.add(i);
+        }
         AllPermutationsGenerator test = new AllPermutationsGenerator();
-        test.printAll(arr);
-        test.heapsRecAlgo(arr.size(), arr);
+
+        long startTime = System.nanoTime();
+        test.heapsRecAlgo(arr);
+        long duration = (System.nanoTime() - startTime);
+        int timeProcessing = (int) (duration / 1000000);
+        System.out.println("Heap's algo: " + timeProcessing);
+
+        startTime = System.nanoTime();
+        test.factoradicAlgo(arr);
+        duration = (System.nanoTime() - startTime);
+        timeProcessing = (int) (duration / 1000000);
+        System.out.println("Factoradic algo: " + timeProcessing);
+
     }
 }
--- src/main/java/Knapsack.java
+++ src/main/java/Knapsack.java
@@ -1,5 +1,7 @@
 public class Knapsack {
+
     public static void main(String[] args) {
-        System.out.println("Hello world");
+        Knapsack knapsack = new Knapsack();
+
     }
 }
