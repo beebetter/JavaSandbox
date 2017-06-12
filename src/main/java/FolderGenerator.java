00edbb69541579c1ab9c8ec520ba8479a9cd39d8
beeBetter <artem.ogarkov@gmail.com>
2017-06-13 01:10:55 +0300

    add FolderGenerator

---
+++ src/main/java/FolderGenerator.java
@@ -0,0 +1,28 @@
+import java.io.File;
+
+public class FolderGenerator {
+    public static String[] getMessages() {
+        String rawMessage = "12, 3, 4.";
+        String[] messages = rawMessage.split(", |\\. |\\.");
+        return messages;
+    }
+    public static void createFolders() {
+        String[] message = getMessages();
+        String path = "C:" + File.separator + "!Sandbox" + File.separator + "OperationBirthDay" + File.separator;
+        String nameOfFile;
+        for (Integer num = 0; num < message.length; )
+            try {
+                File f = new File(path + message[num] + ".txt");
+                f.getParentFile().mkdirs();
+                f.createNewFile();
+                num++;
+                path += num.toString() + File.separator;
+            } catch (Exception e) {
+
+            }
+    }
+    public static void main(String[] args) {
+        createFolders();
+
+    }
+}
